2aacd3e21f7d2412675f70ceb764e200ad009d33
OUATTARA Hayoub Siele Andjigui <hayou.ohsa@gmail.com>
2026-07-03 11:38:42 +0100

    feat : Modification du titre du site

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: 'http://localhost:5025/api',
 })
 
-// Intercepteur : ajoute automatiquement le token JWT à chaque requête
+
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
   if (token) {
