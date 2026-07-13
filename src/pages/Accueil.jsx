e9d2a52ba682641d4151003ad17a98407d402797
OUATTARA Hayoub Siele Andjigui <hayou.ohsa@gmail.com>
2026-07-13 11:03:18 +0100

    feat: Amélioration du site SOUK

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { getStats } from '../services/api'
-import './page'
+import '../page/soukLogo.png'
 import './Accueil.css'
 
 function Accueil() {
